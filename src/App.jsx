10b7eea3020d851decd6a23363388229aa00f25d
edupoly <info.edupoly@gmail.com>
2026-04-15 08:12:54 +0530

    basiccode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import HeroSection from "./HeroSection";
-import Navbar from "./Navbar";
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-      <HeroSection></HeroSection>
-    </>
+    <div>
+      <h1>Hello Main</h1>
+    </div>
   );
 }
 
--- src/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import "./Navbar.css";
-function Navbar() {
-  return (
-    <div className="navbar">
-      <h1>Navbar</h1>
-    </div>
-  );
-}
-export default Navbar;
