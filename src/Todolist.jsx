bba5e4db29f050630c7b7e0fbfb0b6579566edf9
edupoly <info.edupoly@gmail.com>
2026-04-22 08:08:03 +0530

    todolist-setter-callback

---
+++ src/Todolist.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function Todolist() {
+  //state
+  var [todos, setTodos] = React.useState([
+    "goto gym",
+    "get almonds",
+    "pay bills",
+    "complete syllabus",
+  ]);
+  //action
+  function addTodo() {
+    var a = document.getElementById("d1").value;
+    // todos.push(a);//wrong because UI will not render//state is immutable
+    // setTodos([...todos, a]);
+    setTodos((cv) => {
+      return [...cv, a];
+    });
+  }
+  function deleteTodo(index) {
+    // var temp = [...todos];
+    // temp.splice(index, 1);
+    // setTodos([...temp]);
+    setTodos((cv) => {
+      return cv.filter((todo, i) => {
+        if (i !== index) {
+          return true;
+        }
+      });
+    });
+  }
+  //ui
+  return (
+    <div className="border border-2 p-2 m-2 border-primary">
+      <h1>Todolist</h1>
+      <input type="text" id="d1" />
+      <button
+        onClick={() => {
+          addTodo();
+        }}
+      >
+        Add Todo
+      </button>
+      <ul>
+        {todos.map((t, i) => {
+          return (
+            <li>
+              {t}
+              <button
+                onClick={() => {
+                  deleteTodo(i);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Todolist;
