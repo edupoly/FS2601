367fcff745d5c8a42c35a89972ea80bc0c840f22
edupoly <info.edupoly@gmail.com>
2026-04-24 09:06:28 +0530

    refactored-todolist-status

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,11 +3,31 @@ import React from "react";
 function Todo(props) {
   return (
     <li className="shadow m-1 p-2 d-flex align-items-center justify-content-between">
-      <b>
-        {props.children} {props.i}
+      <b className={props.todo.status ? "text-decoration-line-through" : ""}>
+        {props.todo.title} {props.i}
       </b>
       <div>
-        <button className="btn btn-warning btn-sm mx-1 my-1">Done</button>
+        {props.todo.status && (
+          <button
+            className="btn btn-info btn-sm mx-1 my-1"
+            onClick={() => {
+              props.toggleTask(props.i);
+            }}
+          >
+            Undo
+          </button>
+        )}
+        {!props.todo.status && (
+          <button
+            className="btn btn-warning btn-sm mx-1 my-1"
+            onClick={() => {
+              props.toggleTask(props.i);
+            }}
+          >
+            Done
+          </button>
+        )}
+
         <button
           className="btn btn-danger btn-sm mx-1 my-1"
           onClick={() => {
@@ -16,7 +36,6 @@ function Todo(props) {
         >
           Delete
         </button>
-        <button className="btn btn-info btn-sm mx-1 my-1">Undo</button>
       </div>
     </li>
   );
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -2,13 +2,39 @@ import React, { useState } from "react";
 import Todo from "./Todo";
 
 function Todolist() {
+  var [filterType, setFilterType] = React.useState("all");
+
   var [todos, setTodos] = useState([
-    "Play Cricket",
-    "Join Gym",
-    "Goto Goa",
-    "Attend Workshop",
-    "Marketing Team",
+    {
+      title: "Play Cricket",
+      status: true,
+    },
+    {
+      title: "Join Gym",
+      status: true,
+    },
+    {
+      title: "Goto Goa",
+      status: false,
+    },
+    {
+      title: "Attend Workshop",
+      status: true,
+    },
+    {
+      title: "Marketing Team",
+      status: false,
+    },
+    {
+      title: "AI Team Meeting",
+      status: false,
+    },
+    {
+      title: "Consultant Call",
+      status: true,
+    },
   ]);
+  var [filteredTodos, setFilteredTodos] = React.useState([...todos]);
   function deleteTodo(index) {
     // ***with temp array **** //
     // var temp = [...todos];
@@ -26,15 +52,85 @@ function Todolist() {
       });
     });
   }
+  // function doneTask(index) {
+  //   var temp = [...todos];
+  //   temp[index].status = true;
+  //   setTodos([...temp]);
+  // }
+  // function undoTask(index) {
+  //   var temp = [...todos];
+  //   temp[index].status = false;
+  //   setTodos([...temp]);
+  // }
+  function toggleTask(index) {
+    var temp = [...todos];
+    temp[index].status = !temp[index].status;
+    setTodos([...temp]);
+  }
+  function handleFilter(f) {
+    setFilterType(f);
+    if (f === "all") {
+      setFilteredTodos([...todos]);
+    }
+    if (f === "complete") {
+      setFilteredTodos(() => {
+        return todos.filter((t) => t.status);
+      });
+    }
+    if (f === "incomplete") {
+      setFilteredTodos(() => {
+        return todos.filter((t) => !t.status);
+      });
+    }
+  }
   return (
     <div className="border border-2 p-2 m-2 border-success">
       <h1>Todolist</h1>
+      <div className="d-flex">
+        <input
+          type="radio"
+          name="fType"
+          value="all"
+          className="ms-3"
+          onChange={() => {
+            handleFilter("all");
+          }}
+          checked={filterType === "all"}
+        />
+        :All
+        <input
+          type="radio"
+          name="fType"
+          value="completed"
+          className="ms-3"
+          onChange={() => {
+            handleFilter("complete");
+          }}
+        />
+        :Completed
+        <input
+          type="radio"
+          name="fType"
+          value="completed"
+          className="ms-3"
+          onChange={() => {
+            handleFilter("incomplete");
+          }}
+        />
+        :Incomplete
+      </div>
+      <h1>Slected Filter:{filterType}</h1>
       <ul className="p-0 list-unstyled">
-        {todos.map((todo, i) => {
+        {filteredTodos.map((todo, i) => {
           return (
-            <Todo deleteTodoItem={deleteTodo} i={i}>
-              {todo}
-            </Todo>
+            <Todo
+              todo={todo}
+              // doneTask={doneTask}
+              // undoTask={undoTask}
+              toggleTask={toggleTask}
+              deleteTodoItem={deleteTodo}
+              i={i}
+            ></Todo>
           );
         })}
       </ul>
