4c88c2942c2e411bb12aad4d9dba27fedc405e0c
edupoly <info.edupoly@gmail.com>
2026-04-24 08:36:31 +0530

    refactored-todolist

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,10 +3,19 @@ import React from "react";
 function Todo(props) {
   return (
     <li className="shadow m-1 p-2 d-flex align-items-center justify-content-between">
-      <b>{props.children}</b>
+      <b>
+        {props.children} {props.i}
+      </b>
       <div>
         <button className="btn btn-warning btn-sm mx-1 my-1">Done</button>
-        <button className="btn btn-danger btn-sm mx-1 my-1">Delete</button>
+        <button
+          className="btn btn-danger btn-sm mx-1 my-1"
+          onClick={() => {
+            props.deleteTodoItem(props.i);
+          }}
+        >
+          Delete
+        </button>
         <button className="btn btn-info btn-sm mx-1 my-1">Undo</button>
       </div>
     </li>
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -2,14 +2,40 @@ import React, { useState } from "react";
 import Todo from "./Todo";
 
 function Todolist() {
-  var [todos, setTodos] = useState(["Play Cricket", "Join Gym", "Goto Goa"]);
+  var [todos, setTodos] = useState([
+    "Play Cricket",
+    "Join Gym",
+    "Goto Goa",
+    "Attend Workshop",
+    "Marketing Team",
+  ]);
+  function deleteTodo(index) {
+    // ***with temp array **** //
+    // var temp = [...todos];
+    // temp.splice(index, 1);
+    // setTodos([...temp]);
 
+    //using setter callback
+    setTodos((ctodos) => {
+      return ctodos.filter((t, i) => {
+        if (i === index) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+    });
+  }
   return (
     <div className="border border-2 p-2 m-2 border-success">
       <h1>Todolist</h1>
       <ul className="p-0 list-unstyled">
-        {todos.map((todo) => {
-          return <Todo>{todo}</Todo>;
+        {todos.map((todo, i) => {
+          return (
+            <Todo deleteTodoItem={deleteTodo} i={i}>
+              {todo}
+            </Todo>
+          );
         })}
       </ul>
     </div>
